4d2b9b9965dd61fd4c5437c3dfb27c8aca9fed51
Pankaj Makhijani <pankajmakhijani6@gmail.com>
2021-04-29 16:07:15 +0530

    Added Class library used in Namespace Example

---
+++ Mathematical1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematical1
+{
+    public class Basic
+    {
+        public static double PI = 3.141592653589;
+
+        public static double GetPi()
+        {
+            return PI;
+        }
+    }
+}
